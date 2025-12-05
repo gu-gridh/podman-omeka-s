e60ff9d92f2ebd9726f457659477877db5e90341
j4lib <julia.beck@lir.gu.se>
2025-12-05 17:36:13 +0100

    Switch to podman secrets to make it work for both versions. Switch to LTS.

--- instances/ostindiska/docker-entrypoint.sh
+++ instances/ostindiska/docker-entrypoint.sh
@@ -3,14 +3,14 @@
 # Generate database configuration file
 echo "Creating database.ini in /var/www/html/config/ ..."
 rm -f /var/www/html/config/database.ini
-echo "user     = \"${MYSQL_USER}\"" > /var/www/html/config/database.ini
-echo "password = \"${MYSQL_PASSWORD}\"" >> /var/www/html/config/database.ini
-echo "dbname   = \"${MYSQL_DATABASE}\"" >> /var/www/html/config/database.ini
-echo "host     = \"${MYSQL_HOST}\"" >> /var/www/html/config/database.ini
+echo "user     = \"${MARIADB_USER}\"" > /var/www/html/config/database.ini
+echo "password = \"$(cat ${MARIADB_PASSWORD_FILE})\"" >> /var/www/html/config/database.ini
+echo "dbname   = \"${MARIADB_DATABASE}\"" >> /var/www/html/config/database.ini
+echo "host     = \"${MARIADB_HOST}\"" >> /var/www/html/config/database.ini
 echo "Done creating database.ini !"
 
-while ! nc -z "${MYSQL_HOST}" "3306"; do
-  echo "Waiting for database connection at ${MYSQL_HOST}:3306..."
+while ! nc -z "${MARIADB_HOST}" "3306"; do
+  echo "Waiting for database connection at ${MARIADB_HOST}:3306..."
   sleep 1
 done
 
