c3103ffbbff8f5f32de4fbfdf4094f7d82e1566f
j4lib <julia.beck@lir.gu.se>
2026-03-16 17:03:34 +0100

    Fix permissions error in pod.

--- instances/ostindiska/docker-entrypoint.sh
+++ instances/ostindiska/docker-entrypoint.sh
@@ -2,7 +2,6 @@
 
 # fix file permissions
 chmod 775 /var/www/html/files /var/www/html/config
-chgrp 1000 /var/www/html/files /var/www/html/config
 
 # Generate database configuration file
 echo "Creating database.ini in /var/www/html/config/ ..."
