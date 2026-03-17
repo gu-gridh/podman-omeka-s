2421ba49b10228e0df43100104782e3ddb18d502
j4lib <julia.beck@lir.gu.se>
2026-03-17 09:30:26 +0100

    Database.ini creation as secret in volume.

--- instances/ostindiska/docker-entrypoint.sh
+++ instances/ostindiska/docker-entrypoint.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# fix file permissions
-chmod -R 775 /var/www/html/files /var/www/html/config
-
-# Generate database configuration file
-echo "Creating database.ini in /var/www/html/config/ ..."
-rm -f /var/www/html/config/database.ini
-echo "user     = \"${MARIADB_USER}\"" > /var/www/html/config/database.ini
-echo "password = \"${MARIADB_PASSWORD}\"" >> /var/www/html/config/database.ini
-echo "dbname   = \"${MARIADB_DATABASE}\"" >> /var/www/html/config/database.ini
-echo "host     = \"${MARIADB_HOST}\"" >> /var/www/html/config/database.ini
-echo "Done creating database.ini !"
-
 while ! nc -z "${MARIADB_HOST}" "3306"; do
   echo "Waiting for database connection at ${MARIADB_HOST}:3306..."
   sleep 1
@@ -19,7 +7,7 @@ done
 
 echo "Database is up and running!"
 
-service apache2 start
+exec apache2-foreground
 
 tail -F /var/www/html/logs/application.log \
         /var/www/html/logs/sql.log \
